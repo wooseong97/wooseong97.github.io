eaf4ceeeb3658ed86c1d3543e51222f508389b5a
Wooseong Jeong <stk14570@kaist.ac.kr>
2026-05-21 23:13:52 +0900

    Reduce zoomed (magnified) size of publication images via medium-zoom margin

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,7 @@
 // Initialize medium zoom.
 $(document).ready(function () {
   medium_zoom = mediumZoom("[data-zoomable]", {
+    margin: 150, // larger margin => smaller zoomed image
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
   });
 });
